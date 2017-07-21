1968de5e49f9998ac98bd35b096f0238ef0187bd
Don Guckenberger <dguck23@gmail.com>
2017-07-21 10:48:36 -0600

    working copy. next step is to fix the switch for bad commands and .then for movie search

--- jarvis.js
+++ jarvis.js
@@ -4,7 +4,8 @@ var command = process.argv[2];
 var searchTerm = "";
 
 var Twitter = require('twitter');
- 
+
+var omdbKey = keys.omdbKeys.apikey;
 var client = new Twitter({
   consumer_key: keys.twitterKeys.consumer_key,
   consumer_secret: keys.twitterKeys.consumer_secret,
@@ -12,32 +13,41 @@ var client = new Twitter({
   access_token_secret: keys.twitterKeys.access_token_secret
 });
 
-// var Spotify = require('node-spotify-api');
-// console.log(Spotify);
+var Spotify = require('node-spotify-api');
 
+var spotify = new Spotify ({
+  id: keys.spotifyKeys.consumer_key,
+  secret: keys.spotifyKeys.consumer_secret,
 
-// var spotify = new Spotify ({
-//   id: keys.spotifyKeys.consumer_id,
-//   secret: keys.spotifyKeys.consumer_secret,
+});
 
-// });
+// // functions
+// // spotify function
+function spotifySearch() {
+	searchTerm = '"' + process.argv.slice(3).join(" ") + '"';
+	console.log("Searching for " + searchTerm + "...\n\n\n\n\n");
+	spotify
+  .search({ type: 'track', query: searchTerm, limit: 1 })
+  .then(function(response) {
+    console.log(`
 
-// // // functions
-// // // spotify function
-// function spotifySearch() {
-// 	spotify
-//   .search({ type: 'track', query: '"'+searchTerm+'"' })
-//   .then(function(response) {
-//     console.log(response);
-//   })
-//   .catch(function(err) {
-//     return console.log('Error occurred: ' + err);
-//   });
-//   }
+----------------------------------------------------------------------------------------
+			You searched for the song: ${response.tracks.items[0].name} 
+			The Artist is: ${response.tracks.items[0].album.artists[0].name}
+			The Album is: ${response.tracks.items[0].album.name}
+			Click here to hear a clip: ${response.tracks.items[0].external_urls.spotify}
+    	`);
+
+  })
+  .catch(function(err) {
+    return console.log('Error occurred: ' + err);
+  });
+  }
 
 // function for twitter
 function tweetFinder() {
-	searchTerm = process.argv[3];
+	searchTerm = '"' + process.argv.slice(3).join(" ") + '"';
+	console.log("Searching for tweets from " + searchTerm + "...\n\n\n\n\n");
 	var params = {screen_name: searchTerm};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
@@ -51,16 +61,18 @@ for (var i=0; i<tweets.length; i++) {
 // function for omdb
 function movieThis() {
 	var request = require("request");
-	searchTerm = process.argv[3];
+	searchTerm = '"' + process.argv.slice(3).join(" ") + '"';
+	console.log("Searching for " + searchTerm + "...\n\n\n\n\n");
+
 	if (searchTerm === '') {
 		searchTerm = 'Mr. Nobody';
 		console.log("If you haven't seen Mr. Nobody, you should. It's great when you want to Netflix and Chill!");
 		console.log("http://www.imdb.com/title/tt0485947/");
 
 	}
-	console.log(process.argv);
+
 // Then run a request to the OMDB API with the movie specified
-request("http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=" + keys.omdbKey.apiKey +"", function(error, response, body) {
+request(("http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=" + omdbKey), function(error, response, body) {
   // If the request is successful (i.e. if the response status code is 200)
   if (!error && response.statusCode === 200) {
     // Parse the body of the site and recover just the imdbRating
@@ -70,16 +82,16 @@ request("http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=" + k
     	} else {
     console.log(`
 
-Title of the movie.
-	${searchTerm} came out in ${JSON.parse(body).Year}
-	${searchTerm} was produced in ${JSON.parse(body).Country}
-	${searchTerm} uses the following language(s): ${JSON.parse(body).Language}
-	${searchTerm} has an IMDB Rating of ${JSON.parse(body).Ratings[0].Value} out of 10.
-==================
-	Here is the plot for ${searchTerm}: ${JSON.parse(body).Plot}
-==================
-	${searchTerm} stars ${JSON.parse(body).Actors}
-	You can learn more about ${searchTerm} at this Rotten Tomatoes URL: ${JSON.parse(body).Website}
+	${JSON.parse(body).Title} came out in ${JSON.parse(body).Year}
+	${JSON.parse(body).Title} was produced in ${JSON.parse(body).Country}
+	${JSON.parse(body).Title} uses the following language(s): ${JSON.parse(body).Language}
+	${JSON.parse(body).Title} has an IMDB Rating of ${JSON.parse(body).Ratings[0].Value} out of 10.
+=======================================================================================================
+Here is the plot for ${searchTerm}: 
+	${JSON.parse(body).Plot}
+=======================================================================================================
+	${JSON.parse(body).Title} stars: ${JSON.parse(body).Actors}
+	You can learn more about ${JSON.parse(body).Title} at this Rotten Tomatoes URL: ${JSON.parse(body).Website}
     	`);
   }
 }
@@ -87,21 +99,42 @@ Title of the movie.
 }
 
 // function for instructions
-function instructions(){
+function lazyUser() {
 	// console.log("running instructions");
 	fs.readFile("random.txt", "utf8", function(error, data) {
 	if (error) {
 		return console.log(error);
 	}
 		var dataList = data.split(",");
-		console.log(dataList[0]);
-		console.log(dataList[1]);
-
-		command = dataList[0];
+		command = 'spotify-this-song';
 		searchTerm = dataList[1];
-		spotifySearch();
+	console.log("Searching for " + searchTerm + "...\n\n\n\n\n");
+	spotify
+  .search({ type: 'track', query: searchTerm, limit: 1 })
+  .then(function(response) {
+    console.log(`
+
+----------------------------------------------------------------------------------------
+			You searched for the song: ${response.tracks.items[0].name} 
+			The Artist is: ${response.tracks.items[0].album.artists[0].name}
+			The Album is: ${response.tracks.items[0].album.name}
+			Click here to hear a clip: ${response.tracks.items[0].external_urls.spotify}
+    	`);
+
+  })
+  .catch(function(err) {
+    return console.log('Error occurred: ' + err);
+  });
+
 	});
 }
+
+function instructions() {
+	console.log("To activate a search, use the proper command. Review the readme.txt file.");
+  	
+}
+
+
 //calls
  
 switch(command) {
@@ -115,7 +148,9 @@ switch(command) {
     tweetFinder();
     break;
   case "do-what-it-says":
-    instructions();
+    lazyUser();
     break;
-  
+  case (!'movie-this'||!'spotify-this-song'||!'twitter'||!'do-what-it-says'):
+  	instructions();
+  	break;
 }
